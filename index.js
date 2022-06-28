d1a7bd25090263e847de0cb6e14dc97cd3bd909c
Altycon <94148532+Altycon@users.noreply.github.com>
2022-06-27 22:55:04 -0700

    adding update via upload

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const log = console.log;
 
 const toggleMenu = (ev)=>{
-    const nav = document.querySelector('.nav_primary');
-    nav.classList.toggle('open')
+    document.querySelector('.nav_primary').classList.toggle('open');
+    
+    const Menu_Button_Bars = [...document.querySelectorAll('.menu_button-bar')];
+    Menu_Button_Bars.forEach( bar => {
+        bar.classList.toggle('open');
+    });
+};
+const closeMenu =(ev)=>{
+    document.querySelector('.nav_primary').classList.remove('open');
+    const Menu_Button_Bars = [...document.querySelectorAll('.menu_button-bar')];
+    Menu_Button_Bars.forEach( bar => {
+        bar.classList.remove('open');
+    });
 }
 const MenuButton = document.querySelector('[data-menu-button]');
-MenuButton.addEventListener('click', toggleMenu)
+MenuButton.addEventListener('click', toggleMenu);
+const NavLinks = [...document.querySelectorAll('.nav_primary-link')];
+NavLinks.forEach( link => {
+    link.addEventListener('click', closeMenu)
+})
 
 const init = ()=>{
     log('Connected')
